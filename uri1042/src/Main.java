bb9c03ba20bb7c3bee063558e14dd618f46e550f
Matheus <matheuszullo@gmail.com>
2025-03-20 09:47:46 -0300

    Resolução URI 1042

--- uri1042/src/Main.java
+++ uri1042/src/Main.java
@@ -3,11 +3,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int valorA, valorB, valorC, maior, meio, menor;
 
-        valorA = sc.nextInt();
-        valorB = sc.nextInt();
-        valorC = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int originalA = a;
+        int originalB = b;
+        int originalC = c;
+
+        if (a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        if (b > c) {
+            int temp = b;
+            b = c;
+            c = temp;
+        }
+        if (a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        System.out.println();
+
+        System.out.println(originalA);
+        System.out.println(originalB);
+        System.out.println(originalC);
 
         sc.close();
     }
